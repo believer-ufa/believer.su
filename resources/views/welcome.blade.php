28fd36d679d68701fccd85da1a3b8efb598521e1
Roman Akhmadullin <freeman.aka.ahmed@gmail.com>
2017-08-13 01:30:52 +0500

    [feat] описание сайта

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Believer [ufa]</title>
+        <meta name="description" content="Пара слов о Романыче">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
